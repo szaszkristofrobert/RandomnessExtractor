7597387549dbdf826cc1c93d592518e341b1444b
szaszkristofrobert <sz.kristof.r@gmail.com>
2023-03-07 16:42:59 +0100

    Basic layout

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+import gui.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        JFrame frame = new JFrame("App");
+        frame.setContentPane(new App().panelMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
---
+++ src/gui/App.java
@@ -0,0 +1,23 @@
+package gui;
+
+import javax.swing.*;
+
+public class App {
+    public JPanel panelMain;
+    private JPanel panelExtractor;
+    private JPanel panelNIST;
+    private JTextField textFieldExtractorInput;
+    private JButton ExtractorInputBrowseButton;
+    private JTextField textFieldExtractorOutput;
+    private JButton ExtractorOutputBrowseButton;
+    private JButton ExtractorRunButton;
+    private JButton NISTRunButton;
+    private JTextField textFieldNISTInput;
+    private JTextField textFieldNISTOutput;
+    private JButton NISTInputBrowseButton;
+    private JButton NISTOutputBrowseButton;
+    private JComboBox NISTComboBox;
+    private JComboBox ExtractorComboBox;
+    private JPanel ExtractoIFrame;
+    private JPanel NISTIFrame;
+}
