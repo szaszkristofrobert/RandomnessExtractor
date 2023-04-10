b09a0bcb3a5240ec4672a6eca0dbb809fff1ff19
szaszkristofrobert <sz.kristof.r@gmail.com>
2023-04-10 14:39:53 +0200

    IVN has input for number of iterations

--- src/cards/IVNCard.java
+++ src/cards/IVNCard.java
@@ -4,20 +4,48 @@ import file_manager.FileManager;
 import gui.App;
 
 import javax.swing.*;
+import java.awt.*;
 import java.io.*;
 import java.nio.file.Files;
 
 public class IVNCard extends ExtractorCard{
+    private JTextField iField;
     public IVNCard(App app){
         super(app);
         this.n = 3;
         this.label = "IVN";
         JLabel label = new JLabel(this.label);
+        label.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        settingsPanel.setLayout(new BoxLayout(settingsPanel, BoxLayout.Y_AXIS));
+        settingsPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JPanel iPanel = new JPanel();
+        iPanel.setLayout(new BoxLayout(iPanel, BoxLayout.X_AXIS));
+
+        iPanel.add(new JLabel("iterations: "));
+        iField = new JTextField("inf");
+        iField.setMaximumSize(new Dimension(50, 20));
+        iPanel.add(iField);
+
+        settingsPanel.add(iPanel);
+
         this.add(label);
+        this.add(settingsPanel);
         this.add(buttonContinue);
     }
     @Override
     public void execute() {
+        int iterations = -1;
+        try {
+            if (Integer.parseInt(iField.getText()) > 0) {
+                iterations = Integer.parseInt(iField.getText());
+            }
+        }
+        catch (Error e){
+            e.printStackTrace();
+        }
+
         FileManager fm = new FileManager();
         File input = fm.setupInput(inputField.getText());
         File output = fm.setupOutput(input.getName(), outputField.getText(), "_ivn.txt");
@@ -28,7 +56,7 @@ public class IVNCard extends ExtractorCard{
             fm.copyContent(input, iteratingInput);
             FileWriter writer = new FileWriter(output, false);
 
-            while (iteratingInput.length() > 0){
+            while (iteratingInput.length() > 0 && iterations > 0){
                 FileWriter discardedWriter = new FileWriter(discarded, false);
                 FileInputStream fileInput = new FileInputStream(iteratingInput);
                 int r;
@@ -55,11 +83,19 @@ public class IVNCard extends ExtractorCard{
                 discardedWriter.close();
                 fileInput.close();
                 fm.copyContent(discarded, iteratingInput);
+                iterations--;
             }
             writer.close();
         }
         catch (IOException e) {
             System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        try {
+            discarded.delete();
+            iteratingInput.delete();
+        }
+        catch (SecurityException e){
             e.printStackTrace();
         }
     }
